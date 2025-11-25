381f4550192adffc889dd9703098bf6bdb02c2bd
Guilherme Risse <guirisse@hotmail.com>
2025-11-25 11:12:06 -0300

    Versão 25.11

--- projeto_pdfs.py
+++ projeto_pdfs.py
@@ -3,56 +3,11 @@ import pandas as pd
 import gradio as gr
 from google import genai
 from google.genai import types
+from prompt import prompt_model_json
 from dotenv import load_dotenv
 import json 
 import fitz
 from PIL import Image
-
-prompt_model_json = """
-Você é um assistente de IA especializado em extração de dados de notas fiscais.
-
-Entrada: Você receberá um arquivo PDF de uma nota fiscal.
-
-INSTRUÇOES GERAIS: Você deve processar o arquivo PDF e extrair as informações,
-estruturando-as exatamente como descrito abaixo em formato JSON.
-
-CAMPOS A EXTRAIR (Manter a ordem e os nomes exatos):
-    'numero_da_nota',
-    'emitente',
-    'nome_do_destinatario',
-    'icms',
-    'ipi',
-    'total_da_nota',
-    'subtotal_da_nota',
-    'itens'
-
-CAMPOS DE ITENS (Nível de Linha):
-A nota pode ter um ou mais itens. Você deve criar um ARRAY JSON chamado 'itens'.
-Para cada item listado na nota, crie um objeto dentro desse array com:
-    'descricao_do_item',
-    'quantidade',
-    'v_unitario'
-
-INSTRUÇÕES DE PARSING E REGRAS:
-*Formato de Saída Obrigatório*: A saída deve ser um **único objeto JSON**.
-NÃO o coloque dentro de um array []. Siga o exemplo rigorosamente.
-
-*NUMERO DA NOTA*: não incluir a série da nota após o número.
-
-*REMOÇÃO DE PRONOME DE TRATAMENTO*: Caso o nome possua um pronome de tratamento
-em seu inicio (ex: "sra, sr, senhor, dr, etc."), considere apenas o nome.
-
-*INSTRUÇÕES DE EMITENTE*: NUNCA insira o CNPJ no nome do emitente, apenas o nome da empresa
-
-*UNIDADES* Não incluir unidades nas moedas, exemplos "R$".
-
-*DECIMAIS*: USE "." COMO SEPARADOR DECIMAL DOS VALORES DE MOEDA, CASO ESTEJA COM "," VOCê PRECISA SUBSTITUIR
-
-Se um campo não for encontrado, retorne o valor como null.
-
-Se uma nota não tiver itens, retorne o array 'itens' como uma lista vazia [].
-
-"""
 
 load_dotenv()
 
